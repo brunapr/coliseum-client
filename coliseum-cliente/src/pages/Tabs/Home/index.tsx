5375677013d0f385d44ac1a2585e589cf2efac10
sunnyseth <8thseth@gmail.com>
2020-12-16 17:24:22 -0300

    + merge seth_integração e gabi home

--- coliseum-cliente/src/pages/Tabs/Home/index.tsx
+++ coliseum-cliente/src/pages/Tabs/Home/index.tsx
@@ -1,28 +1,135 @@
 import React from 'react';
 
-import { View, Text, Button, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 
-import { Header, Title, SubTitle, Content } from './styles';
+import { View, Text, Button, TextInput, ScrollView } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+
+import { InputLabel, Input } from '../../styles';
+import { Header, Body, p, Scroll, DivTitle, PopularCard, OtherCard, Title, SubTitle, Content, DivImgPopular, DivImgOther  } from './styles';
 import { useState } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { InputBox } from '../../Login/styles';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import { BiSearch } from 'react-icons/bi';
 
-import EventDetails from '../../EventDetails/index';
-import { useNavigation } from '@react-navigation/native';
+Home.navigationOptions = {
+    header: null,
+  };
+
+interface EditData {
+    name: string,
+    email: string,
+    phoneNumber: string,
+    password: string,
+    newPassword: string,
+    passwordConfirmation: string,
+}
 
 export default function Home() {
-    const navigation = useNavigation();
+
+    const { control, getValues, handleSubmit, errors } = useForm({ mode: 'onTouched' });
+    const onSubmit = (data: EditData) => { console.log(data) };
+    const onError = (errors: Object) => { console.log(errors) };
+
+    const [editPassword, setEditPassword] = useState(false);
 
     return(
         <Content>
             <ScrollView>
-                <Header>
-                    <Title>Home</Title>
-                    <SubTitle>Veja os principais eventos</SubTitle>
+                <Header>    
+                    <LinearGradient
+                        colors={['#FF4D00', '#FF9345']}
+                        start = {[0,1]}
+                        end={[1,0]}
+                        style={{
+                            position:'absolute',
+                            width:"100%",
+                            height:"100%",
+                            borderBottomRightRadius:35,
+                            borderBottomLeftRadius: 35,
+                        }}
+                    >
+                        <DivTitle>
+                        <Title>Encontre eventos perto de você</Title>
+                        
+                        <img src="/../../../../assets/group.png" alt=""/>
+                        </DivTitle>
+                        
+                        
+                    </LinearGradient>           
 
+                           
                 </Header>
+                <Body>
+
+                    <SubTitle>Eventos populares</SubTitle>
+
+                    <Scroll
+                        horizontal = {true} 
+                    >
+                        <PopularCard>
+
+                            <DivImgPopular>
+              
+                            </DivImgPopular>
+           
+                        </PopularCard>
+
+                        <PopularCard>
+
+                            <DivImgPopular>
                 
-                <TouchableOpacity onPress={() => navigation.navigate('EventDetails')}>
-                    <Text>CLICA AQUI PRA ABRIR</Text>
-                </TouchableOpacity>
+                            </DivImgPopular>
+                       
+
+                        </PopularCard>
+
+                        <PopularCard>
+                            
+                            <DivImgPopular>
+                
+                            </DivImgPopular>
+                            
+                        </PopularCard>
+                    </Scroll>
+                    
+
+                    <SubTitle>Outros eventos</SubTitle>
+
+                    <Scroll>
+                        < OtherCard>
+                            <DivImgOther>
+
+                            </DivImgOther>
+                        
+                        </OtherCard>
+    
+                        <OtherCard>
+                            <DivImgOther>
+
+                            </DivImgOther>
+                            
+                        </OtherCard>
+
+                        <OtherCard>
+                            <DivImgOther>
+
+                            </DivImgOther>
+                            
+                        </OtherCard>
+
+                    </Scroll>
+
+                    
+
+                    
+                    
+                   
+                </Body>
+              
+               
+
+                
                     
                 
             </ScrollView>
--- coliseum-cliente/src/pages/Tabs/Home/styles.tsx
+++ coliseum-cliente/src/pages/Tabs/Home/styles.tsx
@@ -1,49 +1,131 @@
 import styled from 'styled-components/native';
+import imgEvent from '../../../../assets/Event.png';
+import img from '../../../../assets/image1.png';
 
 export const Content = styled.View `
-    width: 100%;
-    height: 100vh;
-    padding: 8%;
+    width: 100vw;
+    height: 90vh;
+    padding: 0%;
 `;
 
 export const Header = styled.View `
-    width: 100%;
-    height: auto;
+    width: 100vw;
+    height: 20vh;
     display: flex;
-    margin-top: 8%;
+    margin-top: 0%;
     margin-bottom: 10%;
     flex-direction: column;
 `;
 
-export const Title = styled.Text `
-    color: #FF4D00;
-    text-align: center;
-    font-size: 20px;
+export const Body = styled.View `
+    width: 100vw;
+    height: 80vh;
+    display: flex;
+    margin-top: 0%;
+    margin-bottom: 10%;
+    flex-direction: column;
 `;
 
-export const SubTitle = styled.Text `
-    font-weight: bold;
-    font-style: italic;
-    opacity: 0.5;
-    width: 50%;
+export const DivTitle = styled.View `
+    width: 68vw;
+    height: 20vh;
+    display: flex;
+    margin-top: 0%;
+    margin-bottom: 0%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+   
 `;
 
-export const Form = styled.View `
-    width: 100%;
+export const DivImgPopular = styled.View `
+    background-image: url(${imgEvent});
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 78vw;
+    height: 65%;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
+    margin-left: 3%;
+    margin-right: 3%;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
+    border-bottom-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+   
 `;
 
-export const FormBox = styled.View `
-    width: 100%;
+export const DivImgOther = styled.View `
+    background-image: url(${img});
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 20vw;
+    height: 12vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 0px;
+    border-bottom-right-radius: 0px;
+    border-bottom-left-radius: 30px;
+   
+`;
+
+export const Scroll = styled.ScrollView `
+    width: 100vw;
+    height: 90%;
+    margin-left: 2%;
+`;
+
+export const PopularCard = styled.View `
+    background: white;
+    width: 78vw;
+    height: 27vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 3%;
+    margin-left: 5%;
+    margin-right: 6%;
+    margin-bottom: 3%;
+    border-radius:30px;
+`;
+
+export const OtherCard = styled.View `
+    background: white;
+    width: 78vw;
+    height: 12vh;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    margin-top: 3%;
     margin-bottom: 5%;
+    margin-left: 10%;
+    border-radius:30px;
 `;
 
-export const SubmitButton = styled.View `
-    width: 60%;
-    padding: 1%;
-    border-radius: 50px;
-    background: rgb(6,157,177);
-    margin-top: 5%;
+
+export const Title = styled.Text `
+    color: white;
+    text-align: center;
+    font-size: 1.6em;
+    font-weight: bold;
+`;
+
+export const p = styled.Text `
+    color: black;
+    text-align: start;
+    font-size: 1em;
+    font-weight: bold;
+`;
+
+
+export const SubTitle = styled.Text `
+    font-weight: bold;
+    font-size: 1.2em;
+    opacity: 0.5;
+    width: 50%;
+    margin-left: 5%;
 `;
