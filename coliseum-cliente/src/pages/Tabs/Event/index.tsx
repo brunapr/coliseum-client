c7cd6888b71a82b32d95ba0f3e244b01c296ce4f
Gabriela Mattos <gabimattos83@hotmail.com>
2020-12-19 10:02:04 -0300

    + criação de botoes, + importação do componente EventSmallCard na tab Event

--- coliseum-cliente/src/pages/Tabs/Event/index.tsx
+++ coliseum-cliente/src/pages/Tabs/Event/index.tsx
@@ -5,6 +5,8 @@ import { View, Text, Button, ScrollView, Alert} from 'react-native';
 import { Header, Title, SubTitle, Content, DivButtons} from './styles';
 import { useState } from 'react';
 
+import EventSmallCard from '../../../components/EventSmallCard/index';
+
 
 Event.navigationOptions = {
     header: null,
@@ -20,19 +22,23 @@ export default function Event() {
 
                         <Button
                             title="Eventos Atuais"
-                            color="#f2f2f2"
+                       
                             onPress={() => Alert.alert('Simple Button pressed')}
                         />
 
                         <Button
                             title="Eventos Anteriores"
-                            color="#f2f2f2"
+                          
                             onPress={() => Alert.alert('Simple Button pressed')}
                         />
                     </DivButtons>
-                    
 
                 </Header>
+
+
+                <EventSmallCard name= "Encontro de leitores" date="21/04/2021" address="Rua dos bobos, 21 - Madureira" > </EventSmallCard>
+                <EventSmallCard name= "Food Truck" date="30/05/2021" address="Rua dos bobos, 30 - Vista Alegre"> </EventSmallCard>
+                <EventSmallCard name= "Vila Mix" date="01/06/2021" address="Rua dos bobos, 21 - Vila Valqueire"> </EventSmallCard>
             
                 
             </ScrollView>
