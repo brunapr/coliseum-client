815bd0c09299513a5a892764bbc0c529fa299431
Gabriela Mattos <gabimattos83@hotmail.com>
2020-12-20 19:49:40 -0300

    + Estilizando botões, + Funcionalidade de Segmentação entre Eventos Atuais e Eventos Anteriores

--- coliseum-cliente/src/pages/Tabs/Event/index.tsx
+++ coliseum-cliente/src/pages/Tabs/Event/index.tsx
@@ -1,45 +1,93 @@
 import React from 'react';
 
-import { View, Text, Button, ScrollView, Alert} from 'react-native';
+import { View, Text, Button, ScrollView, TouchableHighlight} from 'react-native';
 
-import { Header, Title, SubTitle, Content, DivButtons} from './styles';
+import { Header, Title, SubTitle, Content, DivButtons, Body, CurrentContainer, PreviusContainer, ButtonUnable, ButtonAble, ButtonText, ButtonDelete} from './styles';
 import { useState } from 'react';
 
 import EventSmallCard from '../../../components/EventSmallCard/index';
+import ReactDOM from 'react-dom';
 
 
 Event.navigationOptions = {
     header: null,
   };
 
-export default function Event() {
+export default function Event(props:any) {
+
+    const events = props.eventsChange;
+    const [ isActual,  setIsActual ] = useState(true);
+
+    function changeToActual (){
+        setIsActual(true);
+    }
+
+    function changeToPrevius (){
+        setIsActual(false);
+    }
 
     return(
+
         <Content>
             <ScrollView>
                 <Header>
-                    <DivButtons> 
+                    
 
-                        <Button
-                            title="Eventos Atuais"
-                       
-                            onPress={() => Alert.alert('Simple Button pressed')}
-                        />
+                        {isActual
 
-                        <Button
-                            title="Eventos Anteriores"
-                          
-                            onPress={() => Alert.alert('Simple Button pressed')}
-                        />
-                    </DivButtons>
+                            
+                         ?  <DivButtons>  
+                                <ButtonAble onPress={changeToActual}>
+                                <ButtonText>Eventos Atuais</ButtonText>
+                                </ButtonAble>
+
+                                <ButtonUnable onPress={changeToPrevius}>
+                                <ButtonText>Eventos Anteriores</ButtonText>
+                                </ButtonUnable>
+
+                            </DivButtons>
+
+                         :  <DivButtons>
+                                <ButtonUnable onPress={changeToActual}>
+                                <ButtonText>Eventos Atuais</ButtonText>
+                                </ButtonUnable>
+
+                                <ButtonAble onPress={changeToPrevius}>
+                                <ButtonText>Eventos Anteriores</ButtonText>
+                                </ButtonAble>
+
+                            </DivButtons>
+                        
+                        }
+                        
 
                 </Header>
 
+                <Body >
 
-                <EventSmallCard name= "Encontro de leitores" date="21/04/2021" address="Rua dos bobos, 21 - Madureira" > </EventSmallCard>
-                <EventSmallCard name= "Food Truck" date="30/05/2021" address="Rua dos bobos, 30 - Vista Alegre"> </EventSmallCard>
-                <EventSmallCard name= "Vila Mix" date="01/06/2021" address="Rua dos bobos, 21 - Vila Valqueire"> </EventSmallCard>
-            
+                { isActual
+
+                    ? <CurrentContainer> 
+                       
+                        <EventSmallCard name= "Encontro de leitores" date="21/04/2021" address="Rua dos bobos, 21 - Madureira" > </EventSmallCard> 
+                        <EventSmallCard name= "Food Truck" date="30/05/2021" address="Rua dos bobos, 30 - Vista Alegre"> </EventSmallCard>
+                        <EventSmallCard name= "Vila Mix" date="01/06/2021" address="Rua dos bobos, 21 - Vila Valqueire"> </EventSmallCard>
+
+                    </CurrentContainer>
+
+
+                    : <PreviusContainer> 
+                        
+                        <EventSmallCard name= "Piquenique" date="21/04/2021" address="Rua dos bobos, 21 - Madureira" > </EventSmallCard>
+                        <EventSmallCard name= "Almoço comunitário" date="30/05/2021" address="Rua dos bobos, 30 - Vista Alegre"> </EventSmallCard>
+                        <EventSmallCard name= "Passeio" date="01/06/2021" address="Rua dos bobos, 21 - Vila Valqueire"> </EventSmallCard>
+
+                    </PreviusContainer>
+                }
+
+        
+                </Body>
+               
                 
             </ScrollView>
         </Content>
--- coliseum-cliente/src/pages/Tabs/Event/styles.tsx
+++ coliseum-cliente/src/pages/Tabs/Event/styles.tsx
@@ -12,33 +12,101 @@ export const Header = styled.View `
 
     display: flex;
     flex-direction: row;
+    margin-bottom:3%;
+    
+`;
+
+export const Body = styled.ScrollView `
+    width: 100%;
+    height: 85vh;
+    display: flex;
+    flex-direction: column;
+    
+`;
+
+export const CurrentContainer = styled.View `
+    width: 100%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    
+`;
+
+export const PreviusContainer = styled.View `
+    width: 100%;
+    height: 50%;
+
+    display: flex;
+    flex-direction: column;
     
 `;
 
 export const DivButtons = styled.View `
     width: 100%;
-    height: 15vh;
+    height: 10vh;
 
     display: flex;
     flex-direction: row;
-
-    border: 2px solid blue;
 
     color: black;
     
 `;
 
-
 export const Title = styled.Text `
     color: #FF4D00;
     text-align: center;
-    font-size: 20px;
+    font-size: 18px;
 `;
 
 export const SubTitle = styled.Text `
     font-weight: bold;
     font-style: italic;
-    opacity: 0.5;
+    opacity: 0.;
     width: 50%;
 `;
+
+export const ButtonUnable = styled.TouchableOpacity `
+    width: 50%;
+    height: 100%;
+    background-color: #F2F2F2;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    bottom-bottom: 1px solid red;
+`;
+
+export const ButtonAble = styled.TouchableOpacity `
+    width: 50%;
+    height: 100%;
+    background-color: #E5BCAA;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ButtonDelete = styled.TouchableOpacity `
+    width: 10%;
+    height: 22%;
+    background-color: #32CFE3;
+    border-bottom-left-radius: 50px;
+    border-bottom-right-radius: 50px;
+    border-top-left-radius: 50px;
+    border-top-right-radius: 50px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+
+export const ButtonText = styled.Text `
+    font-size: 18px;
+    color: #FF4D00;
+`;
+
 
