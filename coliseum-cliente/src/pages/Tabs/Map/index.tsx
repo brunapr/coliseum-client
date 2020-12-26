56ccf283325cf2390a236001c344a16b61e74a3a
Seth <8thseth@gmail.com>
2020-12-26 02:00:58 -0300

    + página de mapas parcialmente pronta

--- coliseum-cliente/src/pages/Tabs/Map/index.tsx
+++ coliseum-cliente/src/pages/Tabs/Map/index.tsx
@@ -1,45 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 
-import { View, Text, Button, TextInput, ScrollView } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
+import mapMarker from '../../../../assets/marker.png';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import api from '../../../services/api';
 
-import { InputLabel, Input } from '../../styles';
-import { Header, Title, SubTitle, Content, SubmitButton, Form, FormBox } from './styles';
-import { useState } from 'react';
-
-Map.navigationOptions = {
-    header: null,
-  };
-
-interface EditData {
-    name: string,
-    email: string,
-    phoneNumber: string,
-    password: string,
-    newPassword: string,
-    passwordConfirmation: string,
+interface Event {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
 }
 
-export default function Map() {
+export default function OrphanagesMap() {
+    const [events, setEvents] = useState<Event[]>([]);
+    const navigation = useNavigation();
 
-    const { control, getValues, handleSubmit, errors } = useForm({ mode: 'onTouched' });
-    const onSubmit = (data: EditData) => { console.log(data) };
-    const onError = (errors: Object) => { console.log(errors) };
+    useFocusEffect(() => {
+        api.get('api/events').then(response => {
+            setEvents(response.data);
+        })
+    });
 
-    const [editPassword, setEditPassword] = useState(false);
+    function handleNavigateToEventDetails(id: number) {
+        navigation.navigate('EventDetails', { id });
+    }
 
-    return(
-        <Content>
-            <ScrollView>
-                <Header>
-                    <Title>Mapa</Title>
-                    <SubTitle>Selecione no map</SubTitle>
-                </Header>
+    return (
+        <View style={styles.container}>
+          <MapView
+              provider={PROVIDER_GOOGLE}
+              style={styles.map}
+              initialRegion={{
+                  latitude: -22.8897679,
+                  longitude: -43.3749809,
+                  latitudeDelta: 0.008,
+                  longitudeDelta: 0.008,
+              }}
+          >
 
-                
-                    
-                
-            </ScrollView>
-        </Content>
+            {
+                events.map(event => {
+                    return (
+                        <Marker
+                            key={event.id}
+                            icon={mapMarker}
+                            calloutAnchor={{
+                                x: 2.8,
+                                y: 0.8,
+                            }}
+                            coordinate={{
+                                latitude: event.latitude,
+                                longitude: event.longitude,
+                            }}
+                        >
+                          <Callout tooltip onPress={() => {handleNavigateToEventDetails(event.id)}}>
+                            <View style={styles.calloutContainer}>
+                              <Text style={styles.calloutText}>{event.name}</Text>
+                            </View>
+                          </Callout>
+                        </Marker>
+                    );
+                })
+            }
+            
+          </MapView>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+
+    calloutContainer: {
+        width: 160,
+        height: 46,
+        paddingHorizontal: 16,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 16,
+        justifyContent: 'center',
+    },
+
+    calloutText: {
+        color: '#0089a5',
+        fontFamily: 'Nunito_700Bold',
+        fontSize: 14,
+    },
+
+    footer: {
+        position: 'absolute',
+        left: 24,
+        right: 24,
+        bottom: 32,
+
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        height: 56,
+        paddingLeft: 24,
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+        elevation: 3,
+    },
+
+    footerText: {
+        color: '#8fa7b3',
+        fontFamily: 'Nunito_700Bold'
+    },
+
+    createOrphanageButton: {
+        width: 56,
+        height: 56,
+        backgroundColor: '#15c3d6',
+        borderRadius: 20,
+
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
